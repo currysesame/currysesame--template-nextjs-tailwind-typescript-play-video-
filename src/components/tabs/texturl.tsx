ea297669e7025eb3147aa3d01542b77f725811a7
currysesame <60995715+currysesame@users.noreply.github.com>
2022-11-14 21:19:17 +0800

    Add files via upload

---
+++ src/components/tabs/texturl.tsx
@@ -0,0 +1,14 @@
+export const textUrl = { 
+  "day1": {
+	  "text1" : "/textday1_1.txt",
+  	"text2" : "/textday1_2.txt",
+  },
+  "day2": {
+  	"text1" : "/textday2_1.txt",
+  	"text2" : "/textday2_2.txt",
+  },
+  "day3": {
+    "text1" : "/textday3_1.txt",
+    "text2" : "/textday3_2.txt",
+  },
+}
